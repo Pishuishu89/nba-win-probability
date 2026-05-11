564747afa79a05778cdb7704b1fa04b7826fddd9
Ishaan Das-Basak <ishaandasbasak@gmail.com>
2026-05-11 14:46:49 -0400

    add render deployment config

--- run.py
+++ run.py
@@ -47,4 +47,5 @@ if __name__ == "__main__":
 
     t = threading.Thread(target=broadcast_loop, daemon=True)
     t.start()
-    socketio.run(app, host="0.0.0.0", port=5050, debug=False)
+    port = int(os.environ.get("PORT", 5050))
+    socketio.run(app, host="0.0.0.0", port=port, debug=False)
